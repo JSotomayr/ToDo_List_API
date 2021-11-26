a2f5ad4b00541a0d7b86c56f4a6fee9e470ff944
JSotomayr <jaime.sotomayor.rodriguez@gmail.com>
2021-11-26 11:31:17 +0000

    upload exercise

--- src/main.py
+++ src/main.py
@@ -6,9 +6,10 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from sqlalchemy import exc
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -25,19 +26,90 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_task():
+    users = User.get_all()
+    all_users = [user.to_dict() for user in users]
+    return {"msg": "Nice"}, 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def create_user():
+    new_nick = request.json.get('nick', None)
+
+    if not nick:
+        return jsonify({'error': 'Missing user'}), 400
+
+    user_created = User(nick = new_nick, _is_active = True)
+
+    try:
+        user_created.create_user()
+    except exc.IntegrityError:
+        return jsonify({'error': 'Fail in creating task'}), 400
+
+
+@app.route('/user/<int:id>/task', methods=['GET'])
+def get_all_tasks():
+    tasks = Task.get_all()
+    all_tasks = [task.to_dict() for task in tasks]
+    return {"msg": "Nice"}, 200
+
+
+@app.route('/user/<int:id>/task', methods=['GET'])
+def get_user_task(id):
+    user = User.get_by_id(id)
+
+    if user:
+        tasks = Task.get_by_user(id)
+        tasks_dict = [task.to_dict() for task in tasks]
+
+    return jsonify({'error': 'Fail in creating task'}), 400
+
+
+@app.route('/user/<int:id>/task', methods=['POST'])
+def create_user_task(id):
+    user = User.get_by_id(id)
+    task = request.json.get('task', None)
+
+    if user and task:
+        task = Task(task=task, done=False, user_id=id)
+
+    try:
+        task.create()
+        return jsonify(task.to_dict()), 201
+
+    except exc.IntegrityError:
+        return jsonify({'error': 'Fail in creating task'}), 400
+    
+    return jsonify
+
+
+@app.route('/user/int:id_user/task/<int:id_task>', methods=['DELETE'])
+def delete_task(id_user, id_task):
+    user = User.get_by_id(id_user)
+
+    if user:
+        task = Task.get_by_id(id_task)
+
+
+        if task:
+            try:
+                Task.delete()
+                return jsonify(task.to_dict()), 200
+            except exc.IntegrityError:
+                        return jsonify({'error': 'Fail in operation'}), 400
+
+        return jsonify({'error': 'Task not found'}), 404
+
+    return jsonify({'error': 'User not found'}), 404
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,94 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__: "user"
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nick = db.Column(db.String(120), unique=False, nullable=False)
+    _is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User is {self.USER}, id: {self.id}'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+
+    def to_dict(self):
+        return{
+            'id': self.id,
+            'nick': self.nick,
+            'tasks': [task.to_dict() for task in tasks]
+            }
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    @classmethod
+    def get_by_id(clas, id_user):
+        task = clas.query.filter_by(id = id_user).one_or_none()
+        return task
+
+
+    @classmethod
+    def get_all(cls):
+        users = cls.query.all()
+        return users
+        
+    
+    def delete_user(self):
+        self._is_active=False
+        db.session.commit()
+        return self
+        
+
+    
+
+
+class Task(db.Model):
+    __tablename__: "task"
+
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(120), unique=False, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+    def __repr__(self):
+        return f'Task is {self.task}, {self.done}, from {self.user_id}, id: {self.id}'
+
+   
+    def to_dict(self):
+        return{
+            'task': self.task,
+            'done': self.done,
         }
+
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        
+
+    @classmethod
+    def get_by_id(clas, id):
+        task = clas.query.get(id)
+        return task
+
+   
+    @classmethod
+    def get_by_user(cls, user_id):
+        tasks = cls.query.filter_by(user_id)
+        return tasks
+    
+
+    @classmethod
+    def get_all(cls):
+        users = cls.query.all()
+        return users
+
+
+    def delete_task(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
